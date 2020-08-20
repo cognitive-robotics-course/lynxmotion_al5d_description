2a9ba4da224e6c7f60a8aeafd97260ef607afddc
Vinny <vinny.adjibi@outlook.com>
2020-08-20 13:00:52 +0200

    Merging branch improved. Changes approved (#4)
    * Updating code
    
    * Updated blocks, dynamics and documentation
    
    * Code cleanup
    
    * Making minor changes
    
    * Work in progress
    
    * minor changes
    
    * Updated documentation
    
    * Added dependency
    
    * Updated screenshot
    
    * Services and publishers added
    
    * Updated documentation
    
    * Doc update
    
    * Updated robot base
    
    * Update to documentation
    
    * Minor changes
    
    * Updated documentation
    
    * Updated doc
    
    * Updated elbow angle range
    
    * Changed Joint3 range
    
    * Updated screenshot
    
    * Updated friction parameters
    
    * Updated reset service
    
    * changed Joint5 axis
    
    * Changing dynamics parameter
    
    * Fixed message
    
    * Changed rotation axis
    
    * Troubleshooting PIDs
    
    * Updated base movement
    
    * Updated PID
    
    * Changed arm to elbow
    
    * Updated PID values
    
    * Renamed mesh
    
    * Fixing PID bad values
    
    * Fingers axis updated
    
    * Fingers distance updated
    
    * Checking PID
    
    * Trying new PID values
    
    * Updated robot model
    
    * Updated robot model
    
    * Updated model
    
    * Repositioned joints
    
    * Visualization updated

---
+++ include/lynxmotion_al5d_description/manager.h
@@ -0,0 +1,35 @@
+#include <cstring>
+#include <vector>
+#include <thread>
+#include <ros/ros.h>
+#include "lynxmotion_al5d_description/brick.h"
+#include "std_srvs/Empty.h"
+#include "lynxmotion_al5d_description/SpawnBrick.h"
+#include "lynxmotion_al5d_description/KillBrick.h"
+#include "gazebo_msgs/SpawnModel.h"
+#include "gazebo_msgs/DeleteModel.h"
+#include "std_msgs/Float64MultiArray.h"
+
+#define ROBOT_NAMESPACE "/lynxmotion_al5d"
+#define ROBOT_PARAM_SUFFIX "_lego_description"
+
+class Manager
+{
+public:
+    Manager(ros::NodeHandle& n);
+    static int numSpawned;
+    bool spawnBrick(lynxmotion_al5d_description::SpawnBrick::Request &req,
+                    lynxmotion_al5d_description::SpawnBrick::Response &res);
+    bool killBrick(lynxmotion_al5d_description::KillBrick::Request &req,
+                    lynxmotion_al5d_description::KillBrick::Response &res);
+    bool clear(std_srvs::Empty::Request &req,
+                std_srvs::Empty::Response &res);
+    bool reset(std_srvs::Empty::Request &req,
+                std_srvs::Empty::Response &res);
+    int brickIndex(std::string name);
+private:
+    std::vector<Brick*> bricks;
+    ros::Publisher pub_joints;
+    ros::NodeHandle nh;
+};
+
